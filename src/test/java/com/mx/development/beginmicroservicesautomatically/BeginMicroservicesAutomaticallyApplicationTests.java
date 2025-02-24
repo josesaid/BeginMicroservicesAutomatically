01ef0f35d8ebe90ee286d82f9c72177d4d624fec
Said Olano <josesaid@gmail.com>
2025-02-24 14:25:03 -0600

    First commit

---
+++ src/test/java/com/mx/development/beginmicroservicesautomatically/BeginMicroservicesAutomaticallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.development.beginmicroservicesautomatically;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginMicroservicesAutomaticallyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
